7864f272b4f25e5c18092b397d0aca3013b1f98a
Laura Segouras <lausegouras@gmail.com>
2024-06-04 19:25:06 -0300

    feat: insertion of loadMeatComponent function on script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 import { loadHeroComponent } from "/src/js/hero.js";
 import { loadBrothComponent } from "/src/js/broth.js";
+import { loadMeatComponent } from "/src/js/meat.js";
 
 loadHeroComponent();
 loadBrothComponent();
+loadMeatComponent();
